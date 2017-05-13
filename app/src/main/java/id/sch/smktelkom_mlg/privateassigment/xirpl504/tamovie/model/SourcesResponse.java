c4b2c2bba56afffb22de4f2d127e64677eb48d1a
anggyanisa <anggyanisa5@gmail.com>
2017-05-13 15:57:21 +0700

    Top_Rated

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl504/tamovie/model/SourcesResponse.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl504.tamovie.model;
+
+import java.util.List;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class SourcesResponse {
+
+    public String status;
+    public List<Result> results;
+
+}
