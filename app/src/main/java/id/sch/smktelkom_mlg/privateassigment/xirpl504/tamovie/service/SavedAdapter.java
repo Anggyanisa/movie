5245c5fff512d159bfa04c44ae12e5b0854352ed
anggyanisa <anggyanisa5@gmail.com>
2017-06-13 08:28:47 +0700

    SavedFragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl504/tamovie/service/SavedAdapter.java
@@ -0,0 +1,24 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl504.tamovie.service;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Smktelkom on 6/13/2017.
+ */
+public class SavedAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
